da4e9dfb12ac3a018ef6439ce8aacb72ac8729dd
Abhijith <abhijithnair062@gmail.com>
2024-04-18 20:17:12 -0400

    Integrate a6 with project

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import express from 'express';
-import Hello from "./Hello.js"
+import express from 'express'
+import mongoose from "mongoose";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-import QuizzesRoutes from './Kanbas/quizzes/routes.js';
-import QuestionsRoutes from './Kanbas/questions/routes.js';
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
-CourseRoutes(app);
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+
+
+app.use(express.json());
 AssignmentRoutes(app);
-app.use(express.json());
 ModuleRoutes(app);
-QuizzesRoutes(app);
-QuestionsRoutes(app);
-app.use(express.json());
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
+
 app.listen(process.env.PORT || 4000);
